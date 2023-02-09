bf383113c75600ac08fb45e5d252d0aacd048014
dj422 <dj422@ic.ac.uk>
2023-02-09 16:42:36 +0000

    Flask Extension

--- literature_searcher/query_processor.py
+++ literature_searcher/query_processor.py
@@ -6,10 +6,10 @@ database = {
     "asimov": """Isaac Asimov (2 January 1920 - 6 April 1992) was an
         American writer and professor of Biochemistry, famous for
         his works of hard science fiction and popular science.""",
-    "kia":"""Kia Motors Corporation (Korean: 기아자동차; Hanja: 起亞自動車; RR:""",
-    "toyota":"""Toyota Motor Corporation (トヨタ自動車株式会社, Toyota Jidōsha Kabushiki-gaisha)""",
     "ford":"""Ford Motor Company is an American multinational automaker headquartered in Dearborn, Michigan, a suburb of Detroit.""",
-    "tesla":"""Tesla, Inc. is an American electric vehicle and clean energy company based in Palo Alto, California."""
+    "tesla":"""Tesla, Inc. is an American electric vehicle and clean energy company based in Palo Alto, California.""",
+    "kant" : """"Immanuel Kant was a German philosopher who is considered a central figure in modern philosophy.""",
+    "kia":"""Kia Motors Corporation, commonly known as Kia, is a South Korean multinational automotive manufacturer headquartered in Seoul."""
 }
 
 
